e509ea41b468f6e3107d90522bac5643e589609d
20at1a0557 <123367220+20at1a0557@users.noreply.github.com>
2023-03-29 10:18:35 +0530

    Add files via upload

---
+++ sub.java
@@ -0,0 +1,22 @@
+btnmult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Checking Input First Is Blank Or Not
+                if (txt1.getText().toString().equals("")) {
+                    // Showing Toast (Message)
+                    Toast.makeText(MainActivity.this, "Please Enter Number", Toast.LENGTH_SHORT).show();
+                } else if (txt2.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Please Enter Number", Toast.LENGTH_SHORT).show();
+                }
+
+                // Both Inputs Are Not Blank , Starting Calculation
+                else {
+                    float a, b, c;
+                    a = Float.parseFloat(txt1.getText().toString());
+                    b = Float.parseFloat(txt2.getText().toString());
+                    c = a*b; // Using Third Variable To Store Output Value
+                    result.setText("The Multiplication Result Is " + c);
+
+                }
+            }
+        });
